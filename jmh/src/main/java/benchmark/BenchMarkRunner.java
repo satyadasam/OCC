fe13311a434f39815bbdfd436468035a8c15ab60
satyadasam <30993178+satyadasam@users.noreply.github.com>
2021-04-20 11:06:18 -0400

    Update BenchMarkRunner.java

--- jmh/src/main/java/benchmark/BenchMarkRunner.java
+++ jmh/src/main/java/benchmark/BenchMarkRunner.java
@@ -1,11 +1,20 @@
 package benchmark;
 
 import org.apache.log4j.BasicConfigurator;
+import org.openjdk.jmh.profile.GCProfiler;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.VerboseMode;
 
 public class BenchMarkRunner {
-        public static void main(String[] args) throws Exception {
-            BasicConfigurator.configure();
-            org.openjdk.jmh.Main.main(args);
-        }
-    }
+	public static void main(String[] args) throws Exception {
+		BasicConfigurator.configure();
+
+		Options options = new OptionsBuilder().verbosity(VerboseMode.NORMAL).addProfiler(GCProfiler.class)
+				.jvmArgs("-Xmx10g").build();
+		new Runner(options).run();
+	}
+}
+
 
